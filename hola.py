2c0fa844d005f76eee3b8a5b7a9abc4de170d82a
lpenafielf2-dev <279033461+lpenafielf2-dev@users.noreply.github.com>
2026-04-24 11:30:51 -0500

    saludo update

--- hola.py
+++ hola.py
@@ -1,1 +1,1 @@
-print("Hello World")
+saludo= "Hola"
--- suma.py
+++ suma.py
@@ -1,3 +1,5 @@
+import hola 
+
 a=0
 b=0
 
@@ -6,3 +8,5 @@ suma2=int(input("ingresa otro numero: "))
 
 resultado= suma1 + suma2
 print("el resultado es: ", resultado)
+
+print(hola.saludo)
