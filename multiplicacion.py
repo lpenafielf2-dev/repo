78c5296f006d756f95487a8facc3e034b3d0b495
Melanie Garcia <154344693+MelanieGarciaO@users.noreply.github.com>
2026-04-24 11:43:37 -0500

    cambio 7

--- multiplicacion.py
+++ multiplicacion.py
@@ -1,13 +1,8 @@
 import hola 
-
 a=0
 b=0
-
 suma1=int(input("ingresa un numero: "))
 suma2=int(input("ingresa otro numero: "))
-
 resultado= suma1 * suma2 
-
 print("el resultado es: ", resultado)
-
 print(hola.saludo)
